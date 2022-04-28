da2c70d6a681be6c90fc4cc3969958773ad49218
Châu Minh Phúc <58540065+minhphuc010194@users.noreply.github.com>
2022-04-28 23:59:33 +0700

    read more how to debug

--- lib/main.dart
+++ lib/main.dart
@@ -60,11 +60,11 @@ class _ChatScreenState extends State<ChatScreen> {
       child: Row(
         children:[
           Flexible(child: TextField(
-            controller: _textController,
-            onSubmitted: _handleSubmitted,
-            decoration:
-            const InputDecoration.collapsed(hintText: 'Send a message'),
-          ),
+              controller: _textController,
+              onSubmitted: _handleSubmitted,
+              decoration:
+              const InputDecoration.collapsed(hintText: 'Send a message'),
+            ),
           ),
           Container(
             margin: const EdgeInsets.symmetric(horizontal: 4.0),
@@ -78,50 +78,50 @@ class _ChatScreenState extends State<ChatScreen> {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({Key? key, required this.title}) : super(key: key);
+//   final String title;
+//
+//   @override
+//   State<MyHomePage> createState() => _MyHomePageState();
+// }
+//
+// class _MyHomePageState extends State<MyHomePage> {
+//   int _counter = 0;
+//
+//   void _incrementCounter() {
+//     setState(() {
+//       _counter++;
+//     });
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(widget.title),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             const Text(
+//               'You have pushed the button this many times:',
+//             ),
+//             Text(
+//               '$_counter',
+//               style: Theme.of(context).textTheme.headline4,
+//             ),
+//           ],
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: _incrementCounter,
+//         tooltip: 'Increment',
+//         child: const Icon(Icons.add),
+//       ), // This trailing comma makes auto-formatting nicer for build methods.
+//     );
+//   }
+// }
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
-
-//https://codelabs.developers.google.com/codelabs/flutter#4
+//https://codelabs.developers.google.com/codelabs/flutter#6
