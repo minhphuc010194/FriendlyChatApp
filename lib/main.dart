dc3c35bcb0f7101c826bc04717ac567fc6f4e88c
Châu Minh Phúc <58540065+minhphuc010194@users.noreply.github.com>
2022-04-29 11:12:40 +0700

    add a UI for displaying messages

--- lib/main.dart
+++ lib/main.dart
@@ -39,9 +39,20 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+
+  final List<ChatMessage> _messages = [];
   final _textController = TextEditingController();
+  final FocusNode _focusNode = FocusNode();
+
   void _handleSubmitted(String text){
     _textController.clear();
+    var message = ChatMessage(
+      text: text,
+    );
+    setState(() {
+      _messages.insert(0, message);
+    });
+    _focusNode.requestFocus();
   }
   @override
   Widget build(BuildContext context) {
@@ -49,21 +60,42 @@ class _ChatScreenState extends State<ChatScreen> {
       appBar: AppBar(
         title: Text('FriendlyChat'),
       ),
-      body: _buildTextcomposer(),
+      body: Column(
+        children: [
+          Flexible(
+            child: ListView.builder(
+              padding: const EdgeInsets.all(8.0),
+              reverse: true,
+              itemBuilder: (_, index) => _messages[index],
+              itemCount: _messages.length,
+            ),
+          ),
+          const Divider(height: 1.0),
+          Container(
+            decoration: BoxDecoration(
+              color: Theme.of(context).cardColor
+            ),
+            child:  _buildTextcomposer(),
+          )
+        ],
+      ),
     );
   }
 
   Widget _buildTextcomposer(){
     return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 4.0),
+      margin: const EdgeInsets.symmetric(vertical: 10.0),
 
       child: Row(
         children:[
-          Flexible(child: TextField(
+
+          Flexible(
+            child: TextField(
               controller: _textController,
               onSubmitted: _handleSubmitted,
               decoration:
               const InputDecoration.collapsed(hintText: 'Send a message'),
+              focusNode: _focusNode,
             ),
           ),
           Container(
@@ -77,4 +109,43 @@ class _ChatScreenState extends State<ChatScreen> {
     );
   }
 }
+
+class ChatMessage extends StatelessWidget {
+
+  // const ChatMessage({Key? key}) : super(key: key);
+  const ChatMessage({
+    required this.text,
+    Key? key,
+  }) : super(key: key);
+  final String text;
+
+
+  @override
+  Widget build(BuildContext context) {
+    String _name = 'Your Name';
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            margin: const EdgeInsets.only(right: 16.0),
+            child: CircleAvatar(child: Text(_name[0])),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(_name, style: Theme.of(context).textTheme.headline4),
+              Container(
+                margin: const EdgeInsets.only(top: 5.0),
+                child: Text(text),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
 //https://codelabs.developers.google.com/codelabs/flutter#6
